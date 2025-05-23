b19f4b1c9e6b6370ccd9129072b5c925534a203d
saarow <saarow.dev@gmail.com>
2025-05-23 11:48:20 +0330

    test(tokenizer): f**k vibe coding again

--- tests/test_tokenizer.c
+++ tests/test_tokenizer.c
@@ -8,13 +8,18 @@ void setUp(void) {}
 void tearDown(void) {}
 
 TokenizerCtx create_ctx(const char *input, size_t input_length) {
-    return (TokenizerCtx){input, input_length, 0, 1, 1};
+    return (TokenizerCtx){
+        .input = input,
+        .input_length = input_length,
+        .pos = 0,
+        .line = 1,
+        .column = 1,
+    };
 }
 
 void assert_token(Token actual, TokenType expected_type,
                   const char *expected_value, size_t expected_length,
-                  size_t expected_line, size_t expected_column,
-                  ErrorCode expected_error) {
+                  size_t expected_line, size_t expected_column) {
     TEST_ASSERT_EQUAL(expected_type, actual.type);
     if (expected_value != NULL) {
         TEST_ASSERT_EQUAL_STRING_LEN(expected_value, actual.start,
@@ -23,7 +28,6 @@ void assert_token(Token actual, TokenType expected_type,
     TEST_ASSERT_EQUAL(expected_length, actual.length);
     TEST_ASSERT_EQUAL(expected_line, actual.line);
     TEST_ASSERT_EQUAL(expected_column, actual.column);
-    TEST_ASSERT_EQUAL(expected_error, actual.error);
 }
 
 void test_simple_tokens(void) {
@@ -31,37 +35,125 @@ void test_simple_tokens(void) {
     TokenizerCtx ctx = create_ctx(input, strlen(input));
 
     Token token = next_token(&ctx);
-    assert_token(token, TOKEN_LEFT_BRACE, "{", 1, 1, 1, ERR_NONE);
+    assert_token(token, TOKEN_LEFT_BRACE, "{", 1, 1, 1);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_RIGHT_BRACE, "}", 1, 1, 3, ERR_NONE);
+    assert_token(token, TOKEN_RIGHT_BRACE, "}", 1, 1, 3);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_LEFT_BRACKET, "[", 1, 1, 5, ERR_NONE);
+    assert_token(token, TOKEN_LEFT_BRACKET, "[", 1, 1, 5);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_RIGHT_BRACKET, "]", 1, 1, 7, ERR_NONE);
+    assert_token(token, TOKEN_RIGHT_BRACKET, "]", 1, 1, 7);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_COMMA, ",", 1, 1, 9, ERR_NONE);
+    assert_token(token, TOKEN_COMMA, ",", 1, 1, 9);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_COLON, ":", 1, 1, 11, ERR_NONE);
+    assert_token(token, TOKEN_COLON, ":", 1, 1, 11);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_TRUE, "true", 4, 1, 13, ERR_NONE);
+    assert_token(token, TOKEN_TRUE, "true", 4, 1, 13);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_FALSE, "false", 5, 1, 18, ERR_NONE);
+    assert_token(token, TOKEN_FALSE, "false", 5, 1, 18);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_NULL, "null", 4, 1, 24, ERR_NONE);
+    assert_token(token, TOKEN_NULL, "null", 4, 1, 24);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_EOF, NULL, 0, 1, 28, ERR_NONE);
+    assert_token(token, TOKEN_EOF, NULL, 0, 1, 28);
+}
+
+void test_string_tokens(void) {
+    const char *input = "\"Hello\" \"escaped\\n\" \"unicode\\u0041\" "
+                        "\"quote:\\\"balance is the key\\\"\"";
+    TokenizerCtx ctx = create_ctx(input, strlen(input));
+
+    Token token = next_token(&ctx);
+    assert_token(token, TOKEN_STRING, "\"Hello\"", 7, 1, 1);
 
     token = next_token(&ctx);
-    assert_token(token, TOKEN_EOF, NULL, 0, 1, 28, ERR_NONE);
+    assert_token(token, TOKEN_STRING, "\"escaped\\n\"", 11, 1, 9);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_STRING, "\"unicode\\u0041\"", 15, 1, 21);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_STRING, "\"quote:\\\"balance is the key\\\"\"",
+                 30, 1, 37);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_EOF, NULL, 0, 1, 67);
+}
+
+void test_number_tokens(void) {
+    const char *input = "123 -456 78.9 1e3 1.2e-4";
+    TokenizerCtx ctx = create_ctx(input, strlen(input));
+
+    Token token = next_token(&ctx);
+    assert_token(token, TOKEN_NUMBER, "123", 3, 1, 1);
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_NUMBER, "-456", 4, 1, 5);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_NUMBER, "78.9", 4, 1, 10);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_NUMBER, "1e3", 3, 1, 15);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_NUMBER, "1.2e-4", 6, 1, 19);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_EOF, NULL, 0, 1, 25);
+}
+
+void test_nested_json(void) {
+    const char *json = "{\"a\":{\"b\":{\"c\":123}}}";
+    TokenizerCtx ctx = create_ctx(json, strlen(json));
+
+    Token token = next_token(&ctx);
+    assert_token(token, TOKEN_LEFT_BRACE, "{", 1, 1, 1);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_STRING, "\"a\"", 3, 1, 2);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_COLON, ":", 1, 1, 5);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_LEFT_BRACE, "{", 1, 1, 6);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_STRING, "\"b\"", 3, 1, 7);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_COLON, ":", 1, 1, 10);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_LEFT_BRACE, "{", 1, 1, 11);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_STRING, "\"c\"", 3, 1, 12);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_COLON, ":", 1, 1, 15);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_NUMBER, "123", 3, 1, 16);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_RIGHT_BRACE, "}", 1, 1, 19);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_RIGHT_BRACE, "}", 1, 1, 20);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_RIGHT_BRACE, "}", 1, 1, 21);
+
+    token = next_token(&ctx);
+    assert_token(token, TOKEN_EOF, NULL, 0, 1, 22);
 }
 
 void test_unexpected_character(void) {
@@ -70,167 +162,36 @@ void test_unexpected_character(void) {
     for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++) {
         TokenizerCtx ctx = create_ctx(inputs[i], strlen(inputs[i]));
         Token token = next_token(&ctx);
-        assert_token(token, TOKEN_INVALID, NULL, 0, 1, 1,
-                     ERR_UNEXPECTED_CHARACTER);
+        assert_token(token, TOKEN_INVALID, NULL, 0, 1, 1);
     }
-}
-
-void test_string_tokens(void) {
-    const char *input =
-        "\"Hello\" \"escaped\\n\" \"unicode\\u0041\" \"invalid\\u00X\"";
-    TokenizerCtx ctx = create_ctx(input, strlen(input));
-
-    Token token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"Hello\"", 7, 1, 1, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"escaped\\n\"", 11, 1, 9, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"unicode\\u0041\"", 15, 1, 21,
-                 ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_INVALID, NULL, 0, 1, 49,
-                 ERR_INVALID_UNICODE_ESCAPE);
-}
-
-void test_number_tokens(void) {
-    const char *input = "123 -456 78.9 1e3 1.2e-4 0123";
-    TokenizerCtx ctx = create_ctx(input, strlen(input));
-
-    Token token = next_token(&ctx);
-    assert_token(token, TOKEN_NUMBER, "123", 3, 1, 1, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_NUMBER, "-456", 4, 1, 5, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_NUMBER, "78.9", 4, 1, 10, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_NUMBER, "1e3", 3, 1, 15, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_NUMBER, "1.2e-4", 6, 1, 19, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_INVALID, NULL, 0, 1, 27,
-                 ERR_INVALID_NUMBER_FORMAT);
 }
 
 void test_invalid_numbers(void) {
     const char *input = "007";
     TokenizerCtx ctx = create_ctx(input, strlen(input));
     Token token = next_token(&ctx);
-    assert_token(token, TOKEN_INVALID, NULL, 0, 1, 2,
-                 ERR_INVALID_NUMBER_FORMAT);
+    assert_token(token, TOKEN_INVALID, NULL, 0, 1, 2);
 
     const char *input2 = "1e+";
     TokenizerCtx ctx2 = create_ctx(input2, strlen(input2));
     Token token2 = next_token(&ctx2);
-    assert_token(token2, TOKEN_INVALID, NULL, 0, 1, 4, ERR_INCOMPLETE_EXPONENT);
+    assert_token(token2, TOKEN_INVALID, NULL, 0, 1, 4);
 
     const char *input3 = ".123";
     TokenizerCtx ctx3 = create_ctx(input3, strlen(input3));
     Token token3 = next_token(&ctx3);
-    assert_token(token3, TOKEN_INVALID, NULL, 0, 1, 1,
-                 ERR_UNEXPECTED_CHARACTER);
-}
-
-void test_nested_json(void) {
-    const char *json = "{\"a\":{\"b\":{\"c\":123}}}";
-    TokenizerCtx ctx = create_ctx(json, strlen(json));
-
-    Token token = next_token(&ctx);
-    assert_token(token, TOKEN_LEFT_BRACE, "{", 1, 1, 1, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"a\"", 3, 1, 2, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_COLON, ":", 1, 1, 5, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_LEFT_BRACE, "{", 1, 1, 6, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"b\"", 3, 1, 7, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_COLON, ":", 1, 1, 10, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_LEFT_BRACE, "{", 1, 1, 11, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"c\"", 3, 1, 12, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_COLON, ":", 1, 1, 15, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_NUMBER, "123", 3, 1, 16, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_RIGHT_BRACE, "}", 1, 1, 19, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_RIGHT_BRACE, "}", 1, 1, 20, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_RIGHT_BRACE, "}", 1, 1, 21, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_EOF, NULL, 0, 1, 22, ERR_NONE);
-}
-
-void test_utf8_characters(void) {
-    const char *json =
-        "{\"utf8-emoji\": \"😊\", \"utf8-japanese\": \"こんにちは\"}";
-    TokenizerCtx ctx = create_ctx(json, strlen(json));
-
-    Token token = next_token(&ctx);
-    assert_token(token, TOKEN_LEFT_BRACE, "{", 1, 1, 1, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"utf8-emoji\"", 12, 1, 2, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_COLON, ":", 1, 1, 14, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"😊\"", 6, 1, 16, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_COMMA, ",", 1, 1, 22, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"utf8-japanese\"", 15, 1, 24, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_COLON, ":", 1, 1, 39, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_STRING, "\"こんにちは\"", 17, 1, 41, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_RIGHT_BRACE, "}", 1, 1, 58, ERR_NONE);
-
-    token = next_token(&ctx);
-    assert_token(token, TOKEN_EOF, NULL, 0, 1, 59, ERR_NONE);
+    assert_token(token3, TOKEN_INVALID, NULL, 0, 1, 1);
 }
 
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(test_simple_tokens);
-    RUN_TEST(test_unexpected_character);
     RUN_TEST(test_string_tokens);
     RUN_TEST(test_number_tokens);
-    RUN_TEST(test_invalid_numbers);
     RUN_TEST(test_nested_json);
-    RUN_TEST(test_utf8_characters);
+    RUN_TEST(test_unexpected_character);
+    RUN_TEST(test_invalid_numbers);
 
     return UNITY_END();
 }
